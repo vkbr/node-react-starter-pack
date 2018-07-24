5444f76405b2bf385a26d1f1375209940ce6ce4b
Vivek Kumar Brohma <vivek.brohma@zalando.de>
2018-07-24 16:45:20 +0200

    Added more content in Home component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { hot } from 'react-hot-loader'
 
 const App = () => (
-	<div>Hello!!</div>
+	<div>
+		<h1>
+			{require('../package.json').name}
+		</h1>
+		<p>Entry point: <code>./src/index.js</code></p>
+		<p>Root React component: <code>./src/App.js</code></p>
+	</div>
 );
 
 export default hot(module)(App)
