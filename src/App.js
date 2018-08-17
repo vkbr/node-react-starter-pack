03fe72d412b9c0b6e9317952fe0b7d54f5bdb44f
vivek.brohma <vivek.brohma@gmail.com>
2018-08-18 01:39:04 +0200

    Adding emotion for styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'
 
+import './normalize.css';
+
 const App = () => (
 	<div>
 		<h1>
---
+++ src/normalize.css.js
@@ -0,0 +1,14 @@
+import { css } from 'react-emotion';
+
+const bodyStyle = css`
+	height: 100%;
+	overflow: auto;
+	padding: 0;
+	margin: 0;
+	font-family: Whitney,DIN,Helvetica,Arial,sans-serif;
+	box-sizing: border-box;
+	font-size: 14px;
+	background: #f7f9ff;
+`;
+
+document.body.classList.add(bodyStyle);
